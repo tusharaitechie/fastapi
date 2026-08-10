ccc27630d460abef73277f5472367860bf6c61c9
tusharaitechie <tusharaitechie@gmail.com>
2026-08-10 15:37:47 +0530

    basic Aync api

---
+++ 07.AsyncAPIDemo/async_main.py
@@ -0,0 +1,9 @@
+import asyncio
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/wait")
+async def wait():
+    await asyncio.sleep(3)  # Non-blocking sleep
+    return {"message": "Finished waiting!"}
