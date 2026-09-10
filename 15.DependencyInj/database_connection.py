a1bf3e90adfe45f49f9359786a8f4d1b8ec578be
tusharaitechie <tusharaitechie@gmail.com>
2026-09-10 17:27:26 +0530

    Worked on Dependency Injection

---
+++ 15.DependencyInj/database_connection.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI, Depends
+
+app = FastAPI()
+
+# dependency function
+def get_db():
+    db = {'connection': 'mock_db_connection'}
+    try:
+        yield db
+    finally:
+        db.close()
+
+# endpoint
+@app.get('/home')
+def home(db=Depends(get_db)):
+    return {'db_status': db['connection']}
